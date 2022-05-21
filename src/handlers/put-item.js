fda2e0ebed136cbdd6492e421ca3936414cd09ff
ArmyAntSEC <55052666+ArmyAntSEC@users.noreply.github.com>
2022-05-21 19:43:53 +0200

    More cleanup

--- src/handlers/put-item.js
+++ src/handlers/put-item.js
@@ -1,23 +1,14 @@
-// Create clients and set shared const values outside of the handler.
-
-// Create a DocumentClient that represents the query to add an item
 const dynamodb = require('aws-sdk/clients/dynamodb');
 const docClient = new dynamodb.DocumentClient();
 
-// Get the DynamoDB table name from environment variables
 const tableName = process.env.SAMPLE_TABLE;
 
-/**
- * A simple example includes a HTTP post method to add one item to a DynamoDB table.
- */
 exports.putItemHandler = async (event) => {
     if (event.httpMethod !== 'POST') {
         throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method.`);
     }
-    // All log statements are written to CloudWatch
     console.info('received:', event);
 
-    // Get id and name from the body of the request
     const body = JSON.parse(event.body);
     const id = body.recipientID;
     const name = body.name;
