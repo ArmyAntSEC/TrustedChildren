8e578cbfc0f901d0549a026e4b3d8f14574325d9
ArmyAntSEC <55052666+ArmyAntSEC@users.noreply.github.com>
2022-06-04 23:27:07 +0200

    More SonarLint cleanup

--- Backend/src/handlers/helloWorld.js
+++ Backend/src/handlers/helloWorld.js
@@ -1,14 +1,12 @@
 const verifyStandardKey = require('../utils/apiKeyHandling.js').verifyStandardKey;
 
-exports.helloWorld = async (event, context) => {
+exports.helloWorld = async (event, _context) => {
     verifyStandardKey(event);
 
-    let response = {
+    return {
         'statusCode': 200,
         'body': JSON.stringify({
             message: 'hello world',
         })
-    }
-
-    return response
+    }    
 };
--- Backend/src/handlers/putItems.js
+++ Backend/src/handlers/putItems.js
@@ -6,7 +6,7 @@ const docClient = new dynamodb.DocumentClient();
 const tableName = process.env.SAMPLE_TABLE;
 
 exports.putItemsHandler = async (event) => {
-    return await apiUtilities.handlerWrapper(event, doPutItems);
+    return apiUtilities.handlerWrapper(event, doPutItems);
 }
 async function doPutItems(event) {
     apiUtilities.verifyProperMethod(event, "POST");
@@ -15,7 +15,7 @@ async function doPutItems(event) {
 
     const senderID = body.senderID;
 
-    for (message of body.messages) {
+    for (const message of body.messages) {
         const recipientID = message.recipientID;
         const data = message.data;
 
@@ -27,4 +27,4 @@ async function doPutItems(event) {
         await docClient.put(params).promise();
 
     }
-};
+}
