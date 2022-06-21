3e25706ed60d13a9911826c2db755877bd0dc0e0
ArmyAntSEC <55052666+ArmyAntSEC@users.noreply.github.com>
2022-06-21 11:50:30 +0200

    Renamed the positions endpoints.

--- Backend/__tests__/live/roundtrip.live.test.js
+++ Backend/__tests__/live/roundtrip.live.test.js
@@ -2,7 +2,7 @@ const { config } = require("aws-sdk");
 
 const axios = require("axios").default
 const myConfig = require("../config/config.json")
-const baseURL = "https://seqfwj19u3.execute-api.eu-west-1.amazonaws.com/Prod/";
+const baseURL = "https://seqfwj19u3.execute-api.eu-west-1.amazonaws.com/Prod/positions/";
 
 describe('Test roundtrip', () => {
 
