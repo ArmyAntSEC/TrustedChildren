b266ee789f99badab01f7f8ffd7662c057860ec8
ArmyAntSEC <55052666+ArmyAntSEC@users.noreply.github.com>
2022-07-08 23:21:29 +0200

    Simple transaction works now

--- Backend/scripts/testClaimPublicKeyAndUuid.sh
+++ Backend/scripts/testClaimPublicKeyAndUuid.sh
@@ -2,8 +2,7 @@
 echo "------"
 randomKey=$(cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${1:-20} | head -n 1)
 randomUuid=$(cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${1:-20} | head -n 1)
-#item="{\"PK\":{\"S\":\"PUBKEY#RandomPubKey$randomKey\"},\"SK\":{\"S\":\"PUBKEY#RandomPubKey$randomKey\"},\"uuid\":{\"S\":\"RandomUUID$randomUuid\"}}"
-aws dynamodb put-item --table-name UuidAndPublicKeyMap --item file://./scripts/item.json --condition-expression "attribute_not_exists(PK)" --endpoint-url http://localhost:8000
+aws dynamodb transact-write-items --transact-items file://./scripts/item.json --endpoint-url http://localhost:8000
 echo "------"
 aws dynamodb scan --table-name UuidAndPublicKeyMap --endpoint-url http://localhost:8000
 echo "------"
